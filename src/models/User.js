c70a634c07a89a0333cd01721f764266397391e9
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-06-06 19:20:25 -0300

    Encrypt password and add hash lib in dependency file

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const mongoose = require("../database");
 
 const UserSchema = new mongoose.Schema({
@@ -20,6 +21,12 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+UserSchema.pre("save", async function(next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);
